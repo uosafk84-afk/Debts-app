928b5ce4ec295cdb6e35894badcbf1b49cc2d529
uosafk84-afk <uosafk84@gmail.com>
2026-08-31 08:42:24 +0430

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
-const CACHE = 'shop-debts-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'shop-debts-v2';
+const ASSETS = [
+  './', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/firebase/12.16.0/firebase-app-compat.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/firebase/12.16.0/firebase-firestore-compat.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  e.waitUntil(
+    caches.open(CACHE).then(c => Promise.all(
+      ASSETS.map(url => fetch(url, {mode:'cors'}).then(res => res.ok ? c.put(url, res) : null).catch(()=>{}))
+    ))
+  );
   self.skipWaiting();
 });
 
@@ -18,7 +27,7 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => {
       const fetchPromise = fetch(e.request).then(res => {
-        if (res && res.status === 200 && e.request.url.startsWith(self.location.origin)) {
+        if (res && res.status === 200) {
           const clone = res.clone();
           caches.open(CACHE).then(c => c.put(e.request, clone));
         }
